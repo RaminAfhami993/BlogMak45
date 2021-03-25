eaf624bb9772733401fdb0fe7ee1451b3125581c
raminafhami993 <ramin.a@marcopacs.com>
2021-03-25 12:57:00 +0430

    create avatars dir

--- tools/initialization.js
+++ tools/initialization.js
@@ -1,7 +1,12 @@
 const User = require('../models/user')
+const fs = require("fs");
+const path = require("path");
+
 module.exports = (function() {
     // createAdmin();
-    
+    (fs.existsSync(path.join(__dirname, '../public/images')) || fs.mkdirSync(path.join(__dirname, '../public/images')));
+    (fs.existsSync(path.join(__dirname, '../public/images/avatars')) || fs.mkdirSync(path.join(__dirname, '../public/images/avatars')));
+
 })();
 
 // function createAdmin() {
